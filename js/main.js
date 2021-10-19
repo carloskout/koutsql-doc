567d1e6a94f572fd990ed1bdd32d7d9312c77ecc
koutinh <coutinhoco@outlook.com>
2021-10-19 12:41:57 -0300

    renomeando projeto

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 let links = undefined;
+let navBarLinkClicked = undefined;
+
 let currentPage = 'Página inicial';
 let currentPageElem = undefined;
 
@@ -41,12 +43,58 @@ function updateBreadCrumbs() {
     currentPageElem.href = '#link-top';
 }
 
+function updateNavbar(path) {
+
+    /* Method Aux
+     * Identifica qual link do menu lateral será marcado
+     * como ativo. verifica tambem se o accordion esta estendido,
+     * se nao estiver ele o estende
+     * 
+     * O parametro 'id' pode ser tanto um nome de classe com um id
+     */
+    function aux(id) {
+        let parent = document.querySelector(id);
+        let link = parent.querySelector("a[href='" + path + "']");
+
+        if(link) {
+            link.classList.toggle('active');
+            if(parent.classList.remove('collapse')) {
+                parent.classList.add('collapsed');
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    let activeLink = document.querySelector('.active');
+    if(activeLink) {
+        activeLink.classList.remove('active');
+    }
+
+    if(navBarLinkClicked) {
+        //checa se o evento de click foi disparado por um link
+        // auxiliar. os links auxiliares são os que ficam dentro
+        // do conteudo que servem para redirecionar o usuario
+        // para outras paginas.
+        if (/link-aux/.test(navBarLinkClicked.className)) {
+            if(!aux('#howToUse')) {
+                aux('#lib');
+            }
+        } else {
+            navBarLinkClicked.classList.add('active');
+        }
+    }
+
+}
+
 function resolveRoute(path) {
     request(path)
         .then((content) => {
             updateContent(content);
             updateLinks();
             updateBreadCrumbs();
+            updateNavbar(path);
         })
         .catch(() => {
             resolveRoute('/404');
@@ -60,6 +108,7 @@ function checkRoute(e) {
         let index = e.target.href.lastIndexOf('/');
         path = e.target.href.substring(index);
         currentPage = e.target.title;
+        navBarLinkClicked = e.target;
         resolveRoute(path);
     }
 
