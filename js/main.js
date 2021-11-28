dd499ce47b5fb5a1f5b48e92bf460a438215620b
koutinh <coutinhoco@outlook.com>
2021-11-28 13:12:34 -0300

    definido a estrutura base do menu siderbar

--- js/main.js
+++ js/main.js
@@ -1,6 +1,25 @@
+
 window.addEventListener('load', (e) => {
-    hljs.configure({language: 'php'});
+    sidebarHideEvent();
+    //hljs.configure({language: 'php'});
 });
+
+function sidebarHideEvent() {
+    let sidebar = document.querySelector('.sidebar');
+
+    let btn = document.querySelector('.btn-bar');
+    btn.addEventListener('click', (e) => {
+        sidebar.classList.toggle('sidebar-hide');
+        btn.classList.toggle('btn-sidebar-hide');
+    });
+
+    if(window.matchMedia("(max-width: 767.98px)").matches) {
+        sidebar.classList.toggle('sidebar-hide');
+        btn.classList.toggle('btn-sidebar-hide');
+        console.log('wawes');
+    }
+}
+
 
 
 
