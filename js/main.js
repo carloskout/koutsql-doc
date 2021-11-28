92339517b4e5d94b027e988c185d54bc7c66ddcb
koutinh <coutinhoco@outlook.com>
2021-11-28 14:42:15 -0300

    concluido efeito slide menu lateral mais conteudo central

--- js/main.js
+++ js/main.js
@@ -6,17 +6,19 @@ window.addEventListener('load', (e) => {
 
 function sidebarHideEvent() {
     let sidebar = document.querySelector('.sidebar');
+    let main = document.querySelector('.main-content');
 
     let btn = document.querySelector('.btn-bar');
     btn.addEventListener('click', (e) => {
-        sidebar.classList.toggle('sidebar-hide');
-        btn.classList.toggle('btn-sidebar-hide');
+        sidebar.classList.toggle('hide');
+        btn.classList.toggle('hide');
+        main.classList.toggle('hide');
     });
 
     if(window.matchMedia("(max-width: 767.98px)").matches) {
-        sidebar.classList.toggle('sidebar-hide');
-        btn.classList.toggle('btn-sidebar-hide');
-        console.log('wawes');
+        sidebar.classList.toggle('hide');
+        btn.classList.toggle('hide');
+        main.classList.toggle('hide');
     }
 }
 
