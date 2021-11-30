2f10d75c9bb8b3f193a7a2d064db999e024ec2e4
koutinh <coutinhoco@outlook.com>
2021-11-29 23:05:59 -0300

    atualizado menu sidebar lateral

--- js/main.js
+++ js/main.js
@@ -6,19 +6,19 @@ window.addEventListener('load', (e) => {
 
 function sidebarHideEvent() {
     let sidebar = document.querySelector('.sidebar');
-    let main = document.querySelector('.main-content');
+    let content = document.querySelector('.content');
 
     let btn = document.querySelector('.btn-bar');
     btn.addEventListener('click', (e) => {
         sidebar.classList.toggle('hide');
         btn.classList.toggle('hide');
-        main.classList.toggle('hide');
+        content.classList.toggle('hide');
     });
 
     if(window.matchMedia("(max-width: 767.98px)").matches) {
         sidebar.classList.toggle('hide');
         btn.classList.toggle('hide');
-        main.classList.toggle('hide');
+        content.classList.toggle('hide');
     }
 }
 
